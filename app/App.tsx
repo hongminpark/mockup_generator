e9b78cbd630ec8057c12094827dd14beeff0b717
hongminpark <zhdals1026@gmail.com>
2024-01-08 23:53:03 +0900

    Add call replicate

--- app/App.tsx
+++ app/App.tsx
@@ -1,26 +1,189 @@
 "use client";
 
 import dynamic from "next/dynamic";
+import { useRef, useState } from "react";
 
 const DynamicScene = dynamic(() => import("./components/Scene"), {
     ssr: false,
     loading: () => <p>Loading...</p>,
 });
+const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 
 export default function App() {
+    const [dimensions, setDimensions] = useState({
+        width: 1,
+        height: 2,
+        depth: 3,
+    });
+    const canvasRef = useRef();
+    const [prediction, setPrediction] = useState(null);
+    const [error, setError] = useState(null);
+
+    const downloadImage = () => {
+        const canvas = canvasRef.current;
+        if (!canvas) return;
+        canvas.toBlob((blob) => {
+            const url = URL.createObjectURL(blob);
+            const link = document.createElement("a");
+            link.style.display = "none";
+            link.href = url;
+            link.download = "scene.png";
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        }, "image/png");
+    };
+
+    const exportScene = () => {
+        const canvas = canvasRef.current;
+        if (!canvas) return;
+        canvas.toBlob((blob) => {
+            const reader = new FileReader();
+            reader.readAsDataURL(blob);
+            reader.onloadend = async () => {
+                const base64data = reader.result;
+                try {
+                    const response = await fetch("/api/predictions", {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify({
+                            image: base64data,
+                            prompt: "A white paper box, white background, 4k, uhd, mockup, photorealistic, ",
+                            scheduler: "K_EULER_ANCESTRAL",
+                            num_samples: 4,
+                            guidance_scale: 7.5,
+                            negative_prompt:
+                                "text, anime, cartoon, graphic, text, painting, crayon, graphite, abstract, glitch, deformed, mutated, ugly, disfigured",
+                            num_inference_steps: 20,
+                            adapter_conditioning_scale: 1,
+                            adapter_conditioning_factor: 1,
+                        }),
+                    });
+
+                    let prediction = await response.json();
+                    if (response.status !== 201) {
+                        setError(prediction.detail);
+                        return;
+                    }
+                    setPrediction(prediction);
+
+                    while (
+                        prediction.status !== "succeeded" &&
+                        prediction.status !== "failed"
+                    ) {
+                        await sleep(1000);
+                        const response = await fetch(
+                            "/api/predictions/" + prediction.id
+                        );
+                        prediction = await response.json();
+                        if (response.status !== 200) {
+                            setError(prediction.detail);
+                            return;
+                        }
+                        console.log({ prediction });
+                        setPrediction(prediction);
+                    }
+                } catch (error) {
+                    setError(
+                        "An error occurred while sending the image to the API."
+                    );
+                    console.error(error);
+                }
+            };
+        }, "image/png");
+    };
+
+    const handleDimensionChange = (dimension) => (e) => {
+        setDimensions({ ...dimensions, [dimension]: e.target.value });
+    };
+
+    const handleDimensionBlur = (dimension) => (e) => {
+        const value = parseFloat(e.target.value);
+        setDimensions({
+            ...dimensions,
+            [dimension]: !isNaN(value) ? value.toString() : "0",
+        });
+    };
+
+    const renderDimensionInput = (dimension) => (
+        <div className="flex items-center">
+            <label className="mr-2 w-16">{dimension}</label>
+            <input
+                type="text"
+                value={dimensions[dimension]}
+                onChange={handleDimensionChange(dimension)}
+                onBlur={handleDimensionBlur(dimension)}
+                className="w-12 py-2 px-2 border-b box-border focus:border-black hover:border-black bg-white focus:outline-none text-center"
+                placeholder={
+                    dimension.charAt(0).toUpperCase() + dimension.slice(1)
+                }
+            />
+        </div>
+    );
+
     return (
         <div className="flex h-screen w-screen text-xs text-neutral-900">
             <div className="w-1/2 h-full flex flex-col p-4">
-                <div className="flex flex-col items-center gap-4">
-                    <DynamicScene />
+                <div className="flex flex-col gap-4">
+                    <DynamicScene
+                        dimensions={dimensions}
+                        canvasRef={canvasRef}
+                    />
+                    <div className="flex flex-col gap-2">
+                        {renderDimensionInput("width")}
+                        {renderDimensionInput("height")}
+                        {renderDimensionInput("depth")}
+                    </div>
+                    <button
+                        className="w-max py-2 px-4 border border-black box-border bg-white hover:bg-black hover:text-white"
+                        onClick={exportScene}
+                    >
+                        RENDER
+                    </button>
+                    <button
+                        className="w-max py-2 px-4 border border-black box-border bg-white hover:bg-black hover:text-white"
+                        onClick={downloadImage}
+                    >
+                        DOWNLOAD
+                    </button>
                 </div>
             </div>
             <div className="w-px bg-black h-full" />
             <div className="w-1/2 h-full overflow-y-auto">
-                <div className="text-medium">
-                    <div className="flex flex-row gap-4"></div>
+                <div className="p-4">
+                    <PredictionResults prediction={prediction} />
                 </div>
             </div>
         </div>
     );
 }
+
+const PredictionResults = ({ prediction }) => {
+    if (!prediction || prediction.status !== "succeeded") {
+        return (
+            <div>No results to display or prediction is not successful.</div>
+        );
+    }
+
+    return (
+        <div>
+            <h2>Output</h2>
+            <div className="flex flex-col gap-4">
+                {prediction.output.map((imageUrl, index) => (
+                    <div
+                        key={index}
+                        // className={`${index === 0 ? "hidden" : ""}`}
+                    >
+                        <img
+                            src={imageUrl}
+                            alt={`AI generated white paper box mockup ${index}`}
+                            className="w-max"
+                        />
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
--- app/components/Scene.tsx
+++ app/components/Scene.tsx
@@ -5,110 +5,15 @@ import {
     OrthographicCamera,
 } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import { Suspense, useRef, useState } from "react";
+import { Suspense, useRef } from "react";
 import Loader from "./Loader";
-const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 
-export default function Scene() {
+export default function Scene({ dimensions, canvasRef }) {
     const cameraCenter = [0, 0, 0];
     const ref = useRef();
-    const [prediction, setPrediction] = useState(null);
-    const [error, setError] = useState(null);
-    const [dimensions, setDimensions] = useState({
-        width: 1,
-        height: 2,
-        depth: 3,
-    });
     const springProps = useSpring({
         scale: [dimensions.width, dimensions.height, dimensions.depth],
     });
-    const exportScene = () => {
-        const canvas = document.querySelector("canvas");
-        canvas.toBlob((blob) => {
-            const reader = new FileReader();
-            reader.readAsDataURL(blob);
-            reader.onloadend = async () => {
-                const base64data = reader.result;
-                try {
-                    const response = await fetch("/api/predictions", {
-                        method: "POST",
-                        headers: {
-                            "Content-Type": "application/json",
-                        },
-                        body: JSON.stringify({
-                            image: base64data,
-                            prompt: "A white paper box, white background, 4k, uhd, mockup, photorealistic, ",
-                            scheduler: "K_EULER_ANCESTRAL",
-                            num_samples: 4,
-                            guidance_scale: 7.5,
-                            negative_prompt:
-                                "text, anime, cartoon, graphic, text, painting, crayon, graphite, abstract, glitch, deformed, mutated, ugly, disfigured",
-                            num_inference_steps: 20,
-                            adapter_conditioning_scale: 1,
-                            adapter_conditioning_factor: 1,
-                        }),
-                    });
-
-                    let prediction = await response.json();
-                    if (response.status !== 201) {
-                        setError(prediction.detail);
-                        return;
-                    }
-                    setPrediction(prediction);
-
-                    while (
-                        prediction.status !== "succeeded" &&
-                        prediction.status !== "failed"
-                    ) {
-                        await sleep(1000);
-                        const response = await fetch(
-                            "/api/predictions/" + prediction.id
-                        );
-                        prediction = await response.json();
-                        if (response.status !== 200) {
-                            setError(prediction.detail);
-                            return;
-                        }
-                        console.log({ prediction });
-                        setPrediction(prediction);
-                    }
-                } catch (error) {
-                    setError(
-                        "An error occurred while sending the image to the API."
-                    );
-                    console.error(error);
-                }
-            };
-        }, "image/png");
-    };
-
-    const handleDimensionChange = (dimension) => (event) => {
-        setDimensions({ ...dimensions, [dimension]: event.target.value });
-    };
-
-    const handleDimensionBlur = (dimension) => (event) => {
-        const value = parseFloat(event.target.value);
-        setDimensions({
-            ...dimensions,
-            [dimension]: !isNaN(value) ? value.toString() : "0",
-        });
-    };
-
-    const renderDimensionInput = (dimension) => (
-        <div className="flex items-center">
-            <label className="mr-2 w-16">{dimension}</label>
-            <input
-                type="text"
-                value={dimensions[dimension]}
-                onChange={handleDimensionChange(dimension)}
-                onBlur={handleDimensionBlur(dimension)}
-                className="w-12 py-2 px-2 border-b box-border focus:border-black hover:border-black bg-white focus:outline-none text-center"
-                placeholder={
-                    dimension.charAt(0).toUpperCase() + dimension.slice(1)
-                }
-            />
-        </div>
-    );
 
     return (
         <div className="flex flex-col w-full gap-4">
@@ -122,7 +27,11 @@ export default function Scene() {
                     maxHeight: "512px",
                 }}
             >
-                <Canvas shadows gl={{ preserveDrawingBuffer: true }}>
+                <Canvas
+                    ref={canvasRef}
+                    shadows
+                    gl={{ preserveDrawingBuffer: true }}
+                >
                     <OrthographicCamera
                         makeDefault
                         position={[5, 3, 5]}
@@ -147,17 +56,6 @@ export default function Scene() {
                         </a.mesh>
                     </Suspense>
                 </Canvas>
-            </div>
-            <div className="flex flex-col gap-2">
-                {renderDimensionInput("width")}
-                {renderDimensionInput("height")}
-                {renderDimensionInput("depth")}
-                <button
-                    className="w-max m-4 py-2 px-4 border border-black box-border bg-white hover:bg-black hover:text-white"
-                    onClick={exportScene}
-                >
-                    RENDER
-                </button>
             </div>
         </div>
     );
