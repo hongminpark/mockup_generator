309f2445de2a5a8d87b1d7b1ee723f41d8284c83
hongminpark <zhdals1026@gmail.com>
2024-01-09 16:59:58 +0900

    Refactor box mesh as a separate component

--- app/components/Scene.tsx
+++ app/components/Scene.tsx
@@ -3,26 +3,24 @@ import {
     Environment,
     OrbitControls,
     OrthographicCamera,
+    useTexture,
 } from "@react-three/drei";
-import { Canvas, useLoader } from "@react-three/fiber";
+import { Canvas } from "@react-three/fiber";
 import { Suspense, useMemo, useRef } from "react";
 import * as THREE from "three";
-import { TextureLoader } from "three";
 import Loader from "./Loader";
 
-export default function Scene({ dimensions, canvasRef }) {
-    const cameraCenter = [0, 0, 0];
-    const ref = useRef();
+function BoxMesh({ dimensions }) {
     const springProps = useSpring({
         scale: [dimensions.width, dimensions.height, dimensions.depth],
     });
-    const textures = useLoader(TextureLoader, [
+    const textures = useTexture([
+        "/pixodoxstudios_Digital_Display_--v_5.2_daa39fd6-4f60-4a9d-b5e2-6867a80d7aa5.png",
         "/1.jpg",
         "/2.jpg",
         "/3.jpg",
         "/4.jpg",
         "/5.jpg",
-        "/6.jpg",
     ]);
     const materials = useMemo(
         () =>
@@ -32,6 +30,24 @@ export default function Scene({ dimensions, canvasRef }) {
         [textures]
     );
 
+    return (
+        <a.mesh scale={springProps.scale}>
+            <boxGeometry args={[1, 1, 1]} />
+            <meshStandardMaterial color="white" />
+            {materials.map((material, index) => (
+                // Apply each material to a different side of the box
+                <primitive
+                    key={index}
+                    attach={`material-${index}`}
+                    object={material}
+                />
+            ))}
+        </a.mesh>
+    );
+}
+export default function Scene({ dimensions, canvasRef }) {
+    const cameraCenter = [0, 0, 0];
+    const ref = useRef();
     return (
         <div className="flex flex-col w-full gap-4">
             <div
@@ -55,16 +71,11 @@ export default function Scene({ dimensions, canvasRef }) {
                         zoom={70}
                         onUpdate={(self) => self.lookAt(...cameraCenter)}
                     />
-                    <color attach="background" args={["#ffffff"]} />
+                    {/* <color attach="background" args={["#ffffff"]} /> */}
                     <Environment files="/keyshot.hdr" />
                     <OrbitControls target={cameraCenter} />
-                    <Suspense fallback={<Loader />}>
-                        <a.mesh scale={springProps.scale}>
-                            <boxGeometry args={[1, 1, 1]} />
-                            {/* <meshStandardMaterial color="white" /> */}
-                            <primitive attach="material" object={materials} />
-                        </a.mesh>
-                    </Suspense>
+                    <BoxMesh dimensions={dimensions} />
+                    <Suspense fallback={<Loader />}></Suspense>
                 </Canvas>
             </div>
         </div>
