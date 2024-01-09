b15a510b54bebc32cc34aa1f8471fbc160285fe7
hongminpark <zhdals1026@gmail.com>
2024-01-09 14:53:08 +0900

    Add image texture per cube

--- app/components/Scene.tsx
+++ app/components/Scene.tsx
@@ -4,8 +4,10 @@ import {
     OrbitControls,
     OrthographicCamera,
 } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
-import { Suspense, useRef } from "react";
+import { Canvas, useLoader } from "@react-three/fiber";
+import { Suspense, useMemo, useRef } from "react";
+import * as THREE from "three";
+import { TextureLoader } from "three";
 import Loader from "./Loader";
 
 export default function Scene({ dimensions, canvasRef }) {
@@ -14,6 +16,21 @@ export default function Scene({ dimensions, canvasRef }) {
     const springProps = useSpring({
         scale: [dimensions.width, dimensions.height, dimensions.depth],
     });
+    const textures = useLoader(TextureLoader, [
+        "/1.jpg",
+        "/2.jpg",
+        "/3.jpg",
+        "/4.jpg",
+        "/5.jpg",
+        "/6.jpg",
+    ]);
+    const materials = useMemo(
+        () =>
+            textures.map(
+                (texture) => new THREE.MeshBasicMaterial({ map: texture })
+            ),
+        [textures]
+    );
 
     return (
         <div className="flex flex-col w-full gap-4">
@@ -44,7 +61,8 @@ export default function Scene({ dimensions, canvasRef }) {
                     <Suspense fallback={<Loader />}>
                         <a.mesh scale={springProps.scale}>
                             <boxGeometry args={[1, 1, 1]} />
-                            <meshStandardMaterial color="white" />
+                            {/* <meshStandardMaterial color="white" /> */}
+                            <primitive attach="material" object={materials} />
                         </a.mesh>
                     </Suspense>
                 </Canvas>
